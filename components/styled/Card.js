313260b67118b7d7abac92418f783ccd15d7f15f
Luthfi Adnan Rahmantyo <Luthfi.adnan@gmail.com>
2024-05-20 23:43:20 +0700

    second commit

---
+++ components/styled/Card.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: #444;
+  box-shadow: 0 4px 8 px rgba(0, 0, 0, 0.2);
+  border-radius: 0.5rem;
+  overflow: hidden;
+`;
+
+export default Card;
